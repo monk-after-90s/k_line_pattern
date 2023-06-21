356d5978d7f1505d4726b69b55cac02c5c8ebf4c
90houlaoheshang <907333918@qq.com>
2023-06-21 17:12:23 +0800

    K line number是形态计算器的类属性

--- pattern_calculator/pattern_calculator.py
+++ pattern_calculator/pattern_calculator.py
@@ -8,23 +8,25 @@ import pandas as pd
 
 
 class PatternCalcltor:
+    # K line number
+    bar_num: int = 0
+
     def __init__(self, bars: Iterable[DbBarData]):
         # 转换为pandas对象
         self.bar_df = pd.DataFrame(sorted([model_to_dict(bar) for bar in bars], key=lambda i: i['datetime']))
         # calculator function
         self.cal_func: Callable | None = None
-        # K line number
-        self.bar_num: int = 0
 
     def __call__(self, *args, **kwargs):
         return self.cal_func(*args, **kwargs)
 
 
 class CSFRCalcltor(PatternCalcltor):
+    bar_num = 20
+
     def __init__(self, bars: Iterable[DbBarData]):
         super().__init__(bars)
         self.cal_func: Callable = CSFR
-        self.bar_num = 20
 
     def __call__(self):
         return self.cal_func(self.bar_df[-self.bar_num:])
