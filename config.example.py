7da31ecc8c813e0f736c36f31f7b223acc9e2ada
90houlaoheshang <907333918@qq.com>
2023-06-21 11:23:53 +0800

    配置模板

---
+++ config.example.py
@@ -0,0 +1,12 @@
+# db0
+DB_HOST = "localhost"
+DB_PORT = 3306
+DB_USER = "user"
+DB_PASSWD = 123456
+DB = "alpha_rabit_dev"
+# db1
+DB_HOST1 = "localhost"
+DB_PORT1 = 3306
+DB_USER1 = "user"
+DB_PASSWD1 = 123456
+DB1 = "quant"
