c68c6bbb435489ffd6ed7abea4bb2f05ce2112e7
90houlaoheshang <907333918@qq.com>
2023-06-21 16:54:18 +0800

    形态计算器 完成CSFR

---
+++ pattern_recognizer/pattern_calculator.py
@@ -0,0 +1,30 @@
+"""形态计算器"""
+from typing import Callable, Iterable
+from playhouse.shortcuts import model_to_dict
+from model import DbBarData
+
+from cal_pattern import *
+import pandas as pd
+
+
+class PatternCalcltor:
+    def __init__(self, bars: Iterable[DbBarData]):
+        # 转换为pandas对象
+        self.bar_df = pd.DataFrame(sorted([model_to_dict(bar) for bar in bars], key=lambda i: i['datetime']))
+        # calculator function
+        self.cal_func: Callable | None = None
+        # K line number
+        self.bar_num: int = 0
+
+    def __call__(self, *args, **kwargs):
+        return self.cal_func(*args, **kwargs)
+
+
+class CSFRCalcltor(PatternCalcltor):
+    def __init__(self, bars: Iterable[DbBarData]):
+        super().__init__(bars)
+        self.cal_func: Callable = CSFR
+        self.bar_num = 20
+
+    def __call__(self):
+        return self.cal_func(self.bar_df[-self.bar_num:])
