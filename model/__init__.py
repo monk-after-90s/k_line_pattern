770564316572fc76499070234303381fd7cdcd88
90houlaoheshang <907333918@qq.com>
2023-06-21 14:10:29 +0800

    重命名

--- model/__init__.py
+++ model/__init__.py
@@ -1,2 +1,2 @@
 from .bar import get_or_create_bar_objects, DbBarData
-from .k_pattern import get_or_create_k_shape_objects, KShape, ShapeMatchRecord
+from .k_pattern import get_or_create_k_pattern_objects, KPattern, PatternMatchRecord
