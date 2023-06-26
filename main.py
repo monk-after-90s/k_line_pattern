858f8b323d0bdc618092d9054f15b9a695a594d8
90houlaoheshang <907333918@qq.com>
2023-06-26 15:46:01 +0800

    当前版本暂时不用进程池执行器

--- main.py
+++ main.py
@@ -7,15 +7,13 @@ from apscheduler_job import set_scheduler
 from pattern_bars import query_newest_bars
 from pattern_calculator import pattern_calcltor_calsses
 from apscheduler_job import interval_filter
-from concurrent.futures import ProcessPoolExecutor
 from functools import partial
 
 
-async def job(executor: ProcessPoolExecutor):
+async def job():
     """
     单次调度任务
 
-    :param executor: 进程池执行器，用于多进程调用计算密集型函数
     :return:
     """
     ...
@@ -24,16 +22,13 @@ async def job(executor: ProcessPoolExecutor):
 def main():
     # 事件循环
     loop = asyncio.get_event_loop()
-    # 进程池执行器
-    executor = ProcessPoolExecutor()
     # 异步定时器
-    aioscheduler = set_scheduler(partial(job, executor=executor), loop)
+    aioscheduler = set_scheduler(job, loop)
     try:
         loop.run_forever()
     except:
         logger.info(f"Start gracefully exit")
         aioscheduler.shutdown()
-        executor.shutdown()
 
 
 if __name__ == '__main__':
