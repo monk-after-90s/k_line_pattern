52b62da2d82ef5103780d7ca079a6d7159b78184
90houlaoheshang <907333918@qq.com>
2023-06-26 17:02:10 +0800

    utilities vnpy INTERVALS到秒数的转换和vnpy K线interval标准转换为币安标准

---
+++ utilities.py
@@ -0,0 +1,4 @@
+# vnpy INTERVALS到秒数的转换
+INTERVAL_SECS_MAP = {"d": 86_400, "4h": 14_400, "1h": 3_600, "30m": 1_800}
+# vnpy K线interval标准转换为币安标准
+VNPY_BN_INTERVAL_MAP = {"d": "1d", "4h": "4h", "1h": "1h", "30m": "30m"}
