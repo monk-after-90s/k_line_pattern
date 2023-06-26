b186a88e91614ad3fcc7c3d98e1852a018273e1e
90houlaoheshang <907333918@qq.com>
2023-06-26 09:59:53 +0800

    精简形态计算器的继承结构 使用单例模式

--- pattern_calculator/pattern_calculator.py
+++ pattern_calculator/pattern_calculator.py
@@ -3,109 +3,57 @@ import threading
 from typing import Callable, Iterable
 from playhouse.shortcuts import model_to_dict
 from model import DbBarData
-
+from abc import ABC
 from cal_pattern import *
 import pandas as pd
 
 
-class PatternCalcltor:
+class PatternCalcltor(ABC):
     # K line number
     bar_num: int = 0
+    cal_func: Callable = None
 
-    def __init__(self, bars: Iterable[DbBarData]):
-        # 转换为pandas Dataframe对象
-        self.bar_df = pd.DataFrame(
+    # 单线程单例模式
+    def __new__(cls, *args, **kwargs):
+        if not hasattr(cls, '_instance'):
+            cls._instance = super().__new__(cls)
+        return cls._instance
+
+    def calculate(self, bars: Iterable[DbBarData]):
+        bar_df = pd.DataFrame(
             sorted([model_to_dict(bar) for bar in bars], key=lambda i: i['datetime'])[-self.bar_num:])
-        # calculator function
-        self.cal_func: Callable | None = None
-
-    def __call__(self, *args, **kwargs):
-        return self.cal_func(*args, **kwargs)
+        return self.cal_func(bar_df)
 
 
 class CSFRCalcltor(PatternCalcltor):
     bar_num = 20
-
-    def __init__(self, bars: Iterable[DbBarData]):
-        super().__init__(bars)
-        self.cal_func: Callable = CSFR
-
-    def __call__(self):
-        return self.cal_func(self.bar_df)
+    cal_func: Callable = CSFR
 
 
 class FindDuckHeadCalcltor(PatternCalcltor):
     bar_num = 105
-
-    def __init__(self, bars: Iterable[DbBarData]):
-        super().__init__(bars)
-        self.cal_func: Callable = find_duck_head
-
-    def __call__(self):
-        return self.cal_func(self.bar_df)
-
-
-class ThreeWavesUpCalcltor(PatternCalcltor):
-    _instance_lock = threading.Lock()
-
-    bar_num = 1  # todo 可以处理历史数据
-
-    def __init__(self, bars: Iterable[DbBarData]):
-        super().__init__(bars)
-        self._three_waves_up_ins = ThreeWavesUp()
-        self.cal_func: Callable = self._three_waves_up_ins.update_bar
-
-    def __new__(cls, *args, **kwargs):
-        if not hasattr(cls, '_instance'):
-            with ThreeWavesUpCalcltor._instance_lock:
-                if not hasattr(cls, '_instance'):
-                    ThreeWavesUpCalcltor._instance = super().__new__(cls)
-
-        return ThreeWavesUpCalcltor._instance
-
-    def __call__(self):
-        return self.cal_func(self.bar_df)
+    cal_func: Callable = find_duck_head
 
 
 class WYGDCalcltor(PatternCalcltor):
     bar_num = 2
-
-    def __init__(self, bars: Iterable[DbBarData]):
-        super().__init__(bars)
-        self.cal_func: Callable = WYGD
-
-    def __call__(self):
-        return self.cal_func(self.bar_df)
+    cal_func: Callable = WYGD
 
 
 class DTZDCalcltor(PatternCalcltor):
     bar_num = 2
+    cal_func: Callable = DTZD
 
-    def __init__(self, bars: Iterable[DbBarData]):
-        super().__init__(bars)
-        self.cal_func: Callable = DTZD
 
-    def __call__(self):
-        return self.cal_func(self.bar_df)
+class ThreeWavesUpCalcltor(PatternCalcltor):
+    _three_waves_up_ins = ThreeWavesUp()
+
+    bar_num = 1  # todo 可以处理历史数据
+    cal_func: Callable = _three_waves_up_ins.update_bar
 
 
 class ThreeWavesDownCalcltor(PatternCalcltor):
-    _instance_lock = threading.Lock()
+    _three_waves_down_ins = ThreeWavesDown()
 
     bar_num = 1  # todo 可以处理历史数据
-
-    def __init__(self, bars: Iterable[DbBarData]):
-        super().__init__(bars)
-        self._three_waves_down_ins = ThreeWavesDown()
-        self.cal_func: Callable = self._three_waves_down_ins.update_bar
-
-    def __new__(cls, *args, **kwargs):
-        if not hasattr(cls, '_instance'):
-            with ThreeWavesDownCalcltor._instance_lock:
-                if not hasattr(cls, '_instance'):
-                    ThreeWavesDownCalcltor._instance = super().__new__(cls)
-
-        return ThreeWavesDownCalcltor._instance
-
-    def __call__(self):
-        return self.cal_func(self.bar_df)
+    cal_func: Callable = _three_waves_down_ins.update_bar
