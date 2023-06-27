10f57c8e97540d03894a6ad02e8fa56f770b4be4
90houlaoheshang <907333918@qq.com>
2023-06-27 17:19:59 +0800

    查找PatternCalcltor继承树里的叶子派生类，即是形态计算器的最终实现

--- pattern_calculator/__init__.py
+++ pattern_calculator/__init__.py
@@ -1,9 +1,5 @@
 from typing import List
-
+from utilities import find_leaf_subclasses
 from .pattern_calculator_class import PatternCalcltor
 
-pattern_calcltor_classes: List[type[PatternCalcltor]] = [PatternCalcltor_subclass
-                                                         for PatternCalcltor_subclass in
-                                                         PatternCalcltor.__subclasses__()
-                                                         if PatternCalcltor_subclass.name]
-__all__ = [pattern_calcltor_classes]
+pattern_calcltor_classes: List[type[PatternCalcltor]] = find_leaf_subclasses(PatternCalcltor)
