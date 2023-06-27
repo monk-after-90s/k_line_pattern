766636e8ab54e411dc14828249ce56b7ed0d9f28
90houlaoheshang <907333918@qq.com>
2023-06-27 14:11:03 +0800

    修复：暴漏形态计算器类没有精确筛选

--- pattern_calculator/__init__.py
+++ pattern_calculator/__init__.py
@@ -1,6 +1,9 @@
 from typing import List
 
-from .pattern_calculator_class import PatternCalcltor as _PatternCalcltor
+from .pattern_calculator_class import PatternCalcltor
 
-pattern_calcltor_calsses: List[type[_PatternCalcltor]] = _PatternCalcltor.__subclasses__()
+pattern_calcltor_calsses: List[type[PatternCalcltor]] = [PatternCalcltor_subclass
+                                                         for PatternCalcltor_subclass in
+                                                         PatternCalcltor.__subclasses__()
+                                                         if PatternCalcltor_subclass.name]
 __all__ = [pattern_calcltor_calsses]
