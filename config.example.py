f2a40aa8d46b35956745359d6b1cfa15c4c54ec6
90houlaoheshang <907333918@qq.com>
2023-06-27 17:48:12 +0800

    配置文件模板

--- config.example.py
+++ config.example.py
@@ -2,11 +2,13 @@
 DB_HOST = "localhost"
 DB_PORT = 3306
 DB_USER = "user"
-DB_PASSWD = 123456
+DB_PASSWD = "123456"
 DB = "alpha_rabit_dev"
 # db1
 DB_HOST1 = "localhost"
 DB_PORT1 = 3306
 DB_USER1 = "user"
-DB_PASSWD1 = 123456
-DB1 = "quant"
+DB_PASSWD1 = "123456"
+DB1 = "quant-test"
+# 形态识别的K线interval，遵循vnpy标准
+INTERVALS = ["d", "4h", "1h", "30m"]
