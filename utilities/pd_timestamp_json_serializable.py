6f5b1ef82ffef3646a811c48215079aa4490b0de
90houlaoheshang <907333918@qq.com>
2023-06-29 19:35:00 +0800

    转换datetime时区

--- utilities/pd_timestamp_json_serializable.py
+++ utilities/pd_timestamp_json_serializable.py
@@ -1,7 +1,7 @@
 """使pandas的Timestamp对象可以JSON serializable到peewee model的JSONField"""
 import json
 from playhouse.mysql_ext import JSONField
-
+from .convert_datetime import convert_to_sh, convert_to_utc
 import pandas as pd
 
 
@@ -9,6 +9,10 @@ class TimestampEncoder(json.JSONEncoder):
     def default(self, obj):
         if not isinstance(obj, pd.Timestamp):
             return super().default(obj)
+        # 无时区信息则为北京时区
+        if obj.tzinfo is None:
+            obj = convert_to_sh(obj)
+        obj = convert_to_utc(obj)
         return obj.isoformat()
 
 
