43f9c86fc6600c4a6a2169ca13966c24de500d76
90houlaoheshang <907333918@qq.com>
2023-07-05 09:56:03 +0800

    类型判断

--- generate_pattern.py
+++ generate_pattern.py
@@ -39,28 +39,30 @@ async def cal_and_record_pattern(pattern_calcltor_class: Type[PatternCalcltor],
             {len(symbol_exchange_interval_bars)=}
             {pattern_calcltor_class=}
             """)
-        # recognize_res:入选时间,形态开始时间，匹配度，extra
-        entry_datetime, start_datetime, matching_score = \
-            recognize_res['EntryTime'], recognize_res['StartTime'], recognize_res['MatchingScore']
-        extra = {k: v for k, v in recognize_res.items() if k not in ['EntryTime', 'StartTime', 'MatchingScore']}
-        # 查询形态
-        k_pattern: KPattern = await k_pattern_objects.get(KPattern, KPattern.name == pattern_calcltor_class.name)
-        # 存储匹配结果
-        for symbol_exchange_interval_bar in symbol_exchange_interval_bars:
-            break
-        symbol_type, united_symbol = await symbol_vnpy2united(symbol_exchange_interval_bar.exchange,
-                                                              symbol_exchange_interval_bar.symbol)
-        try:
-            await k_pattern_objects.create(
-                PatternRecognizeRecord,
-                pattern_id=k_pattern.id,
-                symbol_type=symbol_type,
-                symbol=united_symbol,
-                exchange=symbol_exchange_interval_bar.exchange,
-                k_interval=VNPY_BN_INTERVAL_MAP[symbol_exchange_interval_bar.interval],
-                match_score=matching_score,
-                pattern_end=entry_datetime,
-                pattern_start=start_datetime,
-                extra=extra)
-        except peewee.IntegrityError:
-            pass
+
+        if isinstance(recognize_res, dict):
+            # recognize_res:入选时间,形态开始时间，匹配度，extra
+            entry_datetime, start_datetime, matching_score = \
+                recognize_res['EntryTime'], recognize_res['StartTime'], recognize_res['MatchingScore']
+            extra = {k: v for k, v in recognize_res.items() if k not in ['EntryTime', 'StartTime', 'MatchingScore']}
+            # 查询形态
+            k_pattern: KPattern = await k_pattern_objects.get(KPattern, KPattern.name == pattern_calcltor_class.name)
+            # 存储匹配结果
+            for symbol_exchange_interval_bar in symbol_exchange_interval_bars:
+                break
+            symbol_type, united_symbol = await symbol_vnpy2united(symbol_exchange_interval_bar.exchange,
+                                                                  symbol_exchange_interval_bar.symbol)
+            try:
+                await k_pattern_objects.create(
+                    PatternRecognizeRecord,
+                    pattern_id=k_pattern.id,
+                    symbol_type=symbol_type,
+                    symbol=united_symbol,
+                    exchange=symbol_exchange_interval_bar.exchange,
+                    k_interval=VNPY_BN_INTERVAL_MAP[symbol_exchange_interval_bar.interval],
+                    match_score=matching_score,
+                    pattern_end=entry_datetime,
+                    pattern_start=start_datetime,
+                    extra=extra)
+            except peewee.IntegrityError:
+                pass
