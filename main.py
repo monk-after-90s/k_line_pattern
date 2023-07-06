5301243e524d11d7e5f4816de3210972cfc73a91
90houlaoheshang <907333918@qq.com>
2023-07-06 18:23:09 +0800

    使用SQLAlchemy取代async-peewee

--- main.py
+++ main.py
@@ -3,18 +3,17 @@ import asyncio
 import os
 from datetime import datetime, timedelta
 from functools import partial
-from typing import Iterable, List
+from typing import List
 import beeprint
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
-from peewee import fn
+from sqlalchemy import select
 from apscheduler_job import set_scheduler
 from config import INTERVALS
 from generate_pattern import cal_and_record_pattern_mul_pro
 from realtime_recognize import job
 from utilities import handle_sigterm, symbol_vnpy2united, VNPY_BN_INTERVAL_MAP
 from loguru import logger
-from orm import get_or_create_k_pattern_objects, PatternRecognizeRecord, get_or_create_bar_objects, DbBarOverview, \
-    DbBarData
+from orm import PatternRecognizeRecord, Dbbaroverview, bar_asess_fctry, alrb_asess_fctry, Dbbardata
 from utilities import INTERVAL_SECS_MAP as interval_secs_map
 from concurrent.futures import ProcessPoolExecutor
 from pattern_calculator import pattern_calcltor_classes
@@ -80,33 +79,34 @@ async def handle_interval(interval: str, executor: ProcessPoolExecutor, interval
     interval_last_record_e: 整个周期查询完续接日期的事件
     """
     # 获取symbol配置，分配任务
-    bar_objects = await get_or_create_bar_objects()
-    # 获取symbol配置
-    bar_configs: Iterable[DbBarOverview] = await bar_objects.execute(
-        DbBarOverview.select().where(DbBarOverview.interval == interval))
-    # 查询PatternRecognizeRecord最后日期结束的事件 列表
-    last_record_es = []
-    handle_symbol_interval_tasks = []
-    for bar_config in bar_configs:
-        last_record_e = asyncio.Event()
-        handle_symbol_interval_tasks.append(
-            asyncio.create_task(
-                handle_symbol_interval(bar_config.symbol,
-                                       bar_config.exchange,
-                                       bar_config.interval,
-                                       executor,
-                                       last_record_e)))
-        last_record_es.append(last_record_e)
-    # 等待全部"查找续接日期"完成
-    await asyncio.gather(*(last_record_e.wait() for last_record_e in last_record_es))
-    # 触发整个周期查询完续接日期的事件
-    interval_last_record_e.set()
-    return await asyncio.gather(*handle_symbol_interval_tasks)
+    async with bar_asess_fctry() as session:
+        # 获取symbol配置
+        bar_configs: List[Dbbaroverview] = (await session.execute(
+            select(Dbbaroverview).where(Dbbaroverview.interval == interval))).scalars().all()
+
+        # 查询PatternRecognizeRecord最后日期结束的事件 列表
+        last_record_es = []
+        handle_symbol_interval_tasks = []
+        for bar_config in bar_configs:
+            last_record_e = asyncio.Event()
+            handle_symbol_interval_tasks.append(
+                asyncio.create_task(
+                    handle_symbol_interval(bar_config.symbol,
+                                           bar_config.exchange,
+                                           bar_config.interval,
+                                           executor,
+                                           last_record_e)))
+            last_record_es.append(last_record_e)
+        # 等待全部"查找续接日期"完成
+        await asyncio.gather(*(last_record_e.wait() for last_record_e in last_record_es))
+        # 触发整个周期查询完续接日期的事件
+        interval_last_record_e.set()
+        return await asyncio.gather(*handle_symbol_interval_tasks)
 
 
-async def handle_symbol_interval(symbol,
-                                 exchange,
-                                 interval,
+async def handle_symbol_interval(symbol: str,
+                                 exchange: str,
+                                 interval: str,
                                  executor: ProcessPoolExecutor,
                                  last_record_e: asyncio.Event):
     """
@@ -114,70 +114,76 @@ async def handle_symbol_interval(symbol,
 
     last_record_e: 查询PatternRecognizeRecord最后日期结束的事件
     """
-    bar_objects = await get_or_create_bar_objects()
-
     # 格式转换
     symbol_type, united_symbol = await asyncio.create_task(symbol_vnpy2united(exchange,
                                                                               symbol))
-    # 查找续接日期
-    init_bar_datetime: datetime | None = await (await get_or_create_k_pattern_objects()).scalar(
-        PatternRecognizeRecord.select(fn.Max(PatternRecognizeRecord.pattern_end)).where(
-            (PatternRecognizeRecord.exchange == exchange) &
-            (PatternRecognizeRecord.symbol_type == symbol_type) &
-            (PatternRecognizeRecord.symbol == united_symbol) &
-            (PatternRecognizeRecord.k_interval == VNPY_BN_INTERVAL_MAP[interval])))
-    # 查询PatternRecognizeRecord最后日期完成
-    last_record_e.set()
+    async with alrb_asess_fctry() as session:
+        # 查找续接日期
+        init_bar_datetime: datetime | None = (await session.execute(
+            select(PatternRecognizeRecord.patternEnd).where(
+                PatternRecognizeRecord.exchange == exchange,
+                PatternRecognizeRecord.symbol_type == str(symbol_type),
+                PatternRecognizeRecord.symbol == str(united_symbol),
+                PatternRecognizeRecord.kInterval == str(VNPY_BN_INTERVAL_MAP[interval])
+            ).order_by(PatternRecognizeRecord.patternEnd.desc()).limit(1)
+        )).scalar_one_or_none()
 
-    if init_bar_datetime is None:
-        # 柱子的初始datetime
-        init_bar_datetime: datetime = await bar_objects.scalar(
-            DbBarData.select(fn.Min(DbBarData.datetime)).where((DbBarData.symbol == symbol) &
-                                                               (DbBarData.exchange == exchange) &
-                                                               (DbBarData.interval == interval)))
+        # 查询PatternRecognizeRecord最后日期完成
+        last_record_e.set()
+
+    async with bar_asess_fctry() as session:
         if init_bar_datetime is None:
-            logger.error(beeprint.pp({
-                "msg": "没有K线",
-                "symbol": symbol,
-                "exchange": exchange,
-                "interval": interval
-            }, output=False, sort_keys=False))
-            return
+            # 柱子的初始datetime
+            init_bar_datetime: datetime = (await session.execute(
+                select(Dbbardata.datetime).where(Dbbardata.symbol == str(symbol),
+                                                 Dbbardata.exchange == str(exchange),
+                                                 Dbbardata.interval == str(interval)).
+                order_by(Dbbardata.datetime).limit(1)
+            )).scalar_one_or_none()
 
-    # 需要的最大的Kline Bar数量
-    max_bar_num: int = max(pattern_calcltor_class.bar_num for pattern_calcltor_class in pattern_calcltor_classes)
+            if init_bar_datetime is None:
+                logger.error(beeprint.pp({
+                    "msg": "没有K线",
+                    "symbol": symbol,
+                    "exchange": exchange,
+                    "interval": interval
+                }, output=False, sort_keys=False))
+                return
 
-    # 查询、计算、存储循环
-    while True:
-        # 查bars
-        bars: List[DbBarData] = sorted(await bar_objects.execute(
-            DbBarData.select().where((DbBarData.symbol == symbol) &
-                                     (DbBarData.exchange == exchange) &
-                                     (DbBarData.interval == interval) &
-                                     (DbBarData.datetime <= init_bar_datetime)).
-            order_by(DbBarData.datetime.desc()).limit(max_bar_num)
-        ), key=lambda item: item.datetime)
+        # 需要的最大的Kline Bar数量
+        max_bar_num: int = max(pattern_calcltor_class.bar_num for pattern_calcltor_class in pattern_calcltor_classes)
 
-        if not bars:
-            logger.error(beeprint.pp({
-                "msg": "没有K线",
-                "symbol": symbol,
-                "exchange": exchange,
-                "interval": interval
-            }, output=False, sort_keys=False))
-            return
-        else:
-            # 查bar查到了现在
-            if init_bar_datetime is not None and init_bar_datetime > datetime.now():
-                break
-            # 计算并存储pattern_calcltor值
-            for pattern_calcltor_class in pattern_calcltor_classes:
-                # 信号量控制并发
-                await sem.acquire()
-                asyncio.create_task(cal_and_record_pattern_mul_pro(pattern_calcltor_class, bars, executor, sem))
+        # 查询、计算、存储循环
+        while True:
+            # 查bars
+            bars: List[Dbbardata] = sorted(await session.execute(
+                select(Dbbardata).where(Dbbardata.symbol == symbol,
+                                        Dbbardata.exchange == exchange,
+                                        Dbbardata.interval == interval,
+                                        Dbbardata.datetime <= init_bar_datetime).
+                order_by(Dbbardata.datetime.desc()).limit(max_bar_num)
+            ), key=lambda item: item.datetime)
 
-            # datetime步进
-            init_bar_datetime += timedelta(seconds=interval_secs_map[interval])
+            if not bars:
+                logger.error(beeprint.pp({
+                    "msg": "没有K线",
+                    "symbol": symbol,
+                    "exchange": exchange,
+                    "interval": interval
+                }, output=False, sort_keys=False))
+                return
+            else:
+                # 查bar查到了现在
+                if init_bar_datetime is not None and init_bar_datetime > datetime.now():
+                    break
+                # 计算并存储pattern_calcltor值
+                for pattern_calcltor_class in pattern_calcltor_classes:
+                    # 信号量控制并发
+                    await sem.acquire()
+                    asyncio.create_task(cal_and_record_pattern_mul_pro(pattern_calcltor_class, bars, executor, sem))
+
+                # datetime步进
+                init_bar_datetime += timedelta(seconds=interval_secs_map[interval])
 
 
 if __name__ == '__main__':
