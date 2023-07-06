3c6f0fc9e4bd097e66a7caf4d126b9bd3e37af06
90houlaoheshang <907333918@qq.com>
2023-07-06 21:49:54 +0800

    使用SQLAlchemy查询 存储

--- generate_pattern.py
+++ generate_pattern.py
@@ -23,47 +23,47 @@ async def cal_and_record_pattern_mul_pro(pattern_calcltor_class: Type[PatternCal
             break
         symbol_vnpy2united_task = asyncio.create_task(symbol_vnpy2united(symbol_exchange_interval_bar.exchange,
                                                                          symbol_exchange_interval_bar.symbol))
-        async with alrb_asess_fctry() as session:
-            # 查询形态
-            k_pattern_task = asyncio.create_task(session.execute(
-                select(KPattern).where(KPattern.name == str(pattern_calcltor_class.name))))
 
-            # 计算过程的起始时间
-            start = asyncio.get_running_loop().time()
-            # 匹配结果
-            recognize_res = await asyncio.get_running_loop().run_in_executor(executor,
-                                                                             pattern_calcltor_class().calculate,
-                                                                             symbol_exchange_interval_bars)
+        # 计算过程的起始时间
+        start = asyncio.get_running_loop().time()
+        # 匹配结果
+        recognize_res = await asyncio.get_running_loop().run_in_executor(executor,
+                                                                         pattern_calcltor_class().calculate,
+                                                                         symbol_exchange_interval_bars)
 
-            end = asyncio.get_running_loop().time()
-            getattr(logger, "success" if isinstance(recognize_res, dict) else "info")(
-                beeprint.pp({
-                    "bar_briefing": f"{symbol_exchange_interval_bars[0].symbol} {symbol_exchange_interval_bars[0].exchange} {symbol_exchange_interval_bars[0].interval}",
-                    "pattern_calcltor_class": pattern_calcltor_class,
-                    "cost seconds": end - start,
-                    "recognize_res": recognize_res},
-                    output=False,
-                    sort_keys=False))
+        end = asyncio.get_running_loop().time()
+        getattr(logger, "success" if isinstance(recognize_res, dict) else "info")(
+            beeprint.pp({
+                "bar_briefing": f"{symbol_exchange_interval_bars[0].symbol} {symbol_exchange_interval_bars[0].exchange} {symbol_exchange_interval_bars[0].interval}",
+                "pattern_calcltor_class": pattern_calcltor_class,
+                "cost seconds": end - start,
+                "recognize_res": recognize_res},
+                output=False,
+                sort_keys=False))
 
-            if recognize_res is not None:
-                if isinstance(recognize_res, str):
-                    logger.info(f"""
-                    {recognize_res=}
-                    {len(symbol_exchange_interval_bars)=}
-                    {pattern_calcltor_class=}
-                    """)
+        if recognize_res is not None:
+            if isinstance(recognize_res, str):
+                logger.info(f"""
+                {recognize_res=}
+                {len(symbol_exchange_interval_bars)=}
+                {pattern_calcltor_class=}
+                """)
 
-                if isinstance(recognize_res,
-                              dict) and 'EntryTime' in recognize_res and 'StartTime' in recognize_res and 'MatchingScore' in recognize_res:
+            if isinstance(recognize_res,
+                          dict) and 'EntryTime' in recognize_res and 'StartTime' in recognize_res and 'MatchingScore' in recognize_res:
 
+                # 查询形态
+                async with alrb_asess_fctry() as session:
+                    k_pattern = (await session.execute(
+                        select(KPattern).where(KPattern.name == str(pattern_calcltor_class.name)))).scalars().one()
                     # 存储匹配结果
-                    k_pattern = (await k_pattern_task).scalars().one()
                     symbol_type, united_symbol = await symbol_vnpy2united_task
                     # recognize_res:入选时间,形态开始时间，匹配度，extra
                     entry_datetime, start_datetime, matching_score = \
                         recognize_res['EntryTime'], recognize_res['StartTime'], recognize_res['MatchingScore']
                     extra = {k: v for k, v in recognize_res.items() if
                              k not in ['EntryTime', 'StartTime', 'MatchingScore']}
+                async with alrb_asess_fctry() as session:
                     try:
                         async with session.begin():
                             session.add(
@@ -79,8 +79,6 @@ async def cal_and_record_pattern_mul_pro(pattern_calcltor_class: Type[PatternCal
                                     extra=extra
                                 ))
                     except Exception as e:
-                        logger.error("存PatternRecognizeRecord报错:\n" + beeprint.pp(e,
-                                                                                     output=False,
-                                                                                     sort_keys=False))
+                        logger.error("存PatternRecognizeRecord报错:\n" + beeprint.pp(e, output=False, sort_keys=False))
     finally:
         sem.release()
