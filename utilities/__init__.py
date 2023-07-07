4047109ad190f44846b2152512ee36cdc4493794
90houlaoheshang <907333918@qq.com>
2023-07-07 15:17:33 +0800

    Revert "不再用peewee相关的东西"
    This reverts commit 93ce38c986b962da24972b23dd5d0bcc4b62d6db.

--- utilities/__init__.py
+++ utilities/__init__.py
@@ -7,6 +7,7 @@ VNPY_BN_INTERVAL_MAP = {"d": "1d", "4h": "4h", "1h": "1h", "30m": "30m"}
 from .convert_symbol import symbol_vnpy2united, symbol_united2vnpy
 
 from .leaf_derived_class import find_leaf_subclasses
+from .pd_timestamp_json_serializable import TimestampJSONField
 from .convert_datetime import convert_to_sh, convert_to_utc
 from .loop_sigterm import handle_sigterm
 from .find_gcd import find_gcd
---
+++ utilities/pd_timestamp_json_serializable.py
@@ -0,0 +1,24 @@
+"""使pandas的Timestamp对象可以JSON serializable到peewee model的JSONField"""
+import json
+from playhouse.mysql_ext import JSONField
+from .convert_pd_timestamp import convert_to_sh, convert_to_utc
+import pandas as pd
+
+
+class TimestampEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if not isinstance(obj, pd.Timestamp):
+            return super().default(obj)
+        # 无时区信息则为北京时区
+        if obj.tzinfo is None:
+            obj = convert_to_sh(obj)
+        obj = convert_to_utc(obj)
+        return obj.isoformat()
+
+
+class TimestampJSONField(JSONField):
+    """使pandas的Timestamp对象可以JSON serializable到peewee model的JSONField"""
+
+    def db_value(self, value):
+        if value is not None:
+            return json.dumps(value, cls=TimestampEncoder)
